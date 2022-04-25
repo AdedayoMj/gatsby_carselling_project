30a4f2df4f6cdbd2db90ae3f02fbd1653d423d19
Adedayo <majeedaadegboye@gmail.com>
2022-04-25 12:51:15 +0300

    added navbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -9,13 +9,20 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+
+
 import "./layout.css"
+
+import Navbar from  "./Global/Navbar"
 
 interface MyComponentProps {
   children: React.ReactNode
 }
 function Layout({ children }: MyComponentProps ): JSX.Element {
- return <>{children}</>;
+ return <>
+ <Navbar/>
+ {children}
+ </>;
 }
 // const  Layout: React.FC =({ children })=> <>{children}</>
 
